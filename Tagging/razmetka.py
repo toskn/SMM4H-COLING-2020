3ed19adc05f390aad56e885d25dfe9dbe3904190
polyankaglade <off.polyanskaya.a@gmail.com>
2020-05-28 21:12:48 +0300

    upgrade rezmetka

--- Tagging/razmetka.py
+++ Tagging/razmetka.py
@@ -30,20 +30,45 @@ except FileNotFoundError:
     with open(f'{name}_log.txt', 'w', encoding='utf-8') as f:
         f.write('0')
 
+try:
+    _ = open(f'{name}_output.tsv')
+except FileNotFoundError:
+    with open(f'{name}_output.tsv', 'w', encoding='utf-8') as f:
+        f.write('id\tclass\tplus\tminus\tall\n')
+
 with open(f'{name}_log.txt', 'r', encoding='utf-8') as f:
     count = int(f.readlines()[-1])
 print(f'из них вы разметили уже {count}')
+
+print_option = None
+while print_option is None:
+    print_option = input('print <all> or just <minus>? ')
+    if print_option not in ['all', 'minus']:
+        print_option = None
+
 print('чтобы выйти из разметки и сохранить результат введите out')
 
 with open(f'{name}_output.tsv', 'a', encoding='utf-8') as out:
     go = True
     while go:
         entry = corpora[count]
+        comment = entry['comment']
+        plus =  entry['comment_plus']
+        all = []
+        for field in ['comment', 'comment_plus']:
+            text = entry[field]
+            if text is None:
+                continue
+            all.append(text)
+        com_plus = ' '.join(all)
         minus = entry['comment_minus']
         if minus is None:
             count += 1
             print('skipped, ', count)
             continue
+
+        if print_option == 'all':
+            print('->', com_plus)
 
         print('--->', minus)
         tag = input('1 = нет побочки, 2 = побочка, 3 =  не знаю\ntag: ')
@@ -59,16 +84,13 @@ with open(f'{name}_output.tsv', 'a', encoding='utf-8') as out:
             continue
         elif tag == '1' or tag == '2':
             count += 1
-            output = []
-            for field in ['comment', 'comment_plus', 'comment_minus']:
-                text = entry[field]
-                if text is None:
-                    continue
-                output.append(text)
+
+            com_plus = com_plus.replace('\n', ' ')
+            minus = minus.replace('\n', '')
 
             url = entry['url'].split('/')
             entry_id = url[-3] + "_" + url[-1]
-            out.write(f'\n{entry_id}\t{" ".join(output)}\t{int(tag)-1}')
+            out.write(f'{entry_id}\t{int(tag)-1}\t{com_plus}\t{minus}\t{" ".join([com_plus, minus])}\n')
 
             print('tagged, ', count)
 
